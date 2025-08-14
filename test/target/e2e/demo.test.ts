fbad669931e99680d6b1919f8b07ea88bd770e70
William Shepherd <will@codeincolor.io>
2025-08-14 09:39:37 -0500

    Update demo.test.ts

--- test/target/e2e/demo.test.ts
+++ test/target/e2e/demo.test.ts
@@ -1,24 +1,19 @@
-import { expect, test } from '@playwright/test';
+import {expect, test} from '@playwright/test';
 
-test.describe('Demo test suite', () => {
-	test('home page has expected h1', async ({ page }) => {
-		await page.goto('/');
-		await expect(page.locator('h1')).toBeVisible();
-	});
+test.describe('Tests defined within`test.describe()`', () => {
+    test('home page has expected h1', async ({page}) => {
+        await page.goto('/');
+        await expect(page.locator('h1')).toBeVisible();
+    });
 
-	test('home page has expected p', async ({ page }) => {
-		await page.goto('/');
-		await expect(page.locator('p')).toBeVisible();
-	});
+    test('home page has expected p', async ({page}) => {
+        await page.goto('/');
+        await expect(page.locator('p')).toBeVisible();
+    });
 
-	test('home page has expected h2', async ({ page }) => {
-		await page.goto('/');
-		await expect(page.locator('h2')).toBeVisible();
-	});
-
-    // Including this test causes `junit-report-builder` to render incorrect output
-	// test.skip('home page has expected p', async ({ page }) => {
-	// 		await page.goto('/');
-	// 		await expect(page.locator('p')).toBeVisible();
-	// });
+    test('home page has expected h2 (failure intended)', async ({page}) => {
+        await page.goto('/');
+        await expect(page.locator('h2')).toBeVisible();
+    });
 });
+
